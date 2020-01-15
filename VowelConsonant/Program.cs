a42f012e53fa0ed79639d9a0715ab50e2b5dc306
SheClay <mclay2007@gmail.com>
2020-01-15 17:19:19 -0500

    Finished product.

--- VowelConsonant/Program.cs
+++ VowelConsonant/Program.cs
@@ -9,7 +9,24 @@ namespace VowelConsonant
             Console.Write("Please enter a letter: ");
             string userInput = Console.ReadLine();
             char myLetter = Convert.ToChar(userInput);
+
+            //if letter is a,e,i,o,u then it is a vowel.
+            //otherwise say it is a consonant.
+
+            if (myLetter == 'a' || myLetter == 'e' || myLetter == 'i' || myLetter == 'o' || myLetter == 'u')
+            {
+                Console.WriteLine("This is a vowel.");
+            }
+            else
+            {
+                Console.WriteLine("This is a consonant.");
+            }
+
+                
+        
         }
+        
+
     } 
 
 }
