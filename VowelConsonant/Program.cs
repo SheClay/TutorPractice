355127e5c9035c5a1033910b84b2f816118211ab
SheClay <mclay2007@gmail.com>
2020-01-15 16:33:50 -0500

    Initial commit of tutoring exercise

---
+++ VowelConsonant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VowelConsonant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
